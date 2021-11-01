248a23aca141ff0f51dcdbc73b4073644eee3a20
Mehdi Hassan Akash <mhakash211998@gmail.com>
2021-11-01 18:34:23 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/test', (req, res) => {
+  res.json({
+    message: 'This is a test!'
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
